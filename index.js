f6ad71f22b5dcbccdc04d67e5758dd25a80b3335
eldelosnamesxd-create <eldelosnamesxd@gmail.com>
2025-11-12 07:45:19 -0600

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import { connectDB } from "./db.js";
 import { Card } from "./models/card.js";
 
 const app = express();
 connectDB();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
